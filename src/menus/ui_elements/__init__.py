f720f62e63b73890cdd6c560228638c97fd58485
Lucas Zanardo <lucas.zanardo.etu@univ-lille.fr>
2023-06-22 04:32:00 +0200

    Added example menu and improved subpackages managment

--- src/menus/ui_elements/__init__.py
+++ src/menus/ui_elements/__init__.py
@@ -1,2 +1,12 @@
 """ Ui Elements subpackage """
-__all__ = ["button", "textbox", "text_style"]
+
+from src.menus.ui_elements.button import Button
+from src.menus.ui_elements.textbox import TextBox
+from src.menus.ui_elements.textbox_password import TextBoxPassword
+from src.menus.ui_elements.text_style import TextStyle
+
+__all__ = [
+    "Button",
+    "TextBox", "TextBoxPassword",
+    "TextStyle"
+]
