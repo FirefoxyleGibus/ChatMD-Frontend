cf60ee3a371a6d102ed87eec99dc4de989d8b56f
Lucas Zanardo <lucas.zanardo.etu@univ-lille.fr>
2023-06-22 04:03:17 +0200

    Fixed cursor and some alignements issues

--- src/menus/ui_elements/text_style.py
+++ src/menus/ui_elements/text_style.py
@@ -3,13 +3,25 @@ from blessed import Terminal
 class TextStyle():
     """ Class utils to style text """
     @staticmethod
-    def align(terminal: Terminal, align: str, text: str, width: int) -> tuple[str, int]:
-        """ Return aligned text and the offset to be drawn correctly """
+    def align(terminal: Terminal, align: str, text: str, width: int) -> str:
+        """ Return aligned text to be drawn correctly """
         match align:
             case 'left':
-                return terminal.ljust(text, width), -width
+                return terminal.ljust(text, width), 0
             case 'right':
-                return terminal.rjust(text, width), width+1
-            case ('center'|_):
-                return terminal.center(text, width), 0
-        return text,0
+                return terminal.rjust(text, width), width-len(text)+1
+            case 'center':
+                return terminal.center(text, width), width//2-len(text)//2
+        return text, 0
+    
+    @staticmethod
+    def anchor_pos(anchor, width):
+        """ Return the offset pos for a specified anchor """
+        match anchor:
+            case 'left':
+                return -width
+            case 'right':
+                return  width+1
+            case 'center':
+                return  -width//2
+        return 0
--- src/menus/ui_elements/textbox.py
+++ src/menus/ui_elements/textbox.py
@@ -16,16 +16,17 @@ class TextBox(BaseSelectable):
         'up':    1,
     }
 
-    def __init__(self, width: int, placeholder="text ...", prefix="", reverse_background=True, alignement: str = 'center', attachments: dict = None):
-        super().__init__(attachments=attachments, clear_terminal_move=False)
+    def __init__(self, width: int, placeholder="text ...", prefix="", reverse_background=True, anchor='center', align: str = 'center', attach: dict = None):
+        super().__init__(attachments=attach, clear_terminal_move=False)
         self._text = ""
         self._curpos = 0
         self._width = width
-        self._align = alignement
+        self._anchor = anchor
+        self._align = align
         self._placeholder = placeholder
         self._prefix = prefix
         self._reverse = reverse_background
-    
+
     def handle_inputs(self, val: Keystroke, terminal: Terminal):
         ret = super().handle_inputs(val, terminal)
         match val.name:
@@ -65,11 +66,13 @@ class TextBox(BaseSelectable):
     def draw(self, terminal: Terminal, pos_x: int, pos_y: int):
         """ Draw the textbox """
         background = (terminal.reverse if self._reverse else terminal.normal)
-        text, offset_x = TextStyle.align(terminal, self._align, self._get_shown_text(terminal), self._width)
+        text, left_char_pos = TextStyle.align(terminal, self._align, self._get_shown_text(terminal), self._width)
+        offset_x = TextStyle.anchor_pos(self._anchor, self._width)
         formatted = background + text + terminal.normal
         print_at(terminal, pos_x + offset_x, pos_y, formatted)
 
         # Draw cursor
         if len(self._text) != 0 or self.is_selected:
-            cursor_pos = pos_x + len(self._prefix) + self._curpos + offset_x
+            cursor_pos = pos_x + min(self._curpos, self._width - len(self._prefix) - 1) \
+                + len(self._prefix) + offset_x + left_char_pos
             print_at(terminal, cursor_pos, pos_y, background + terminal.blink("_") + terminal.normal)
